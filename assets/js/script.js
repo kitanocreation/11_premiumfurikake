fe6c6f201ee53ba3840928f654a106f2ef4bb20b
sugimotoshoki4959 <sugimoto.shoki.4959@members.co.jp>
2025-04-22 17:24:31 +0900

    最終チェック

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 $(function() {
 
-    console.log('test');
+    // console.log('test');
+
+    $(function() {
+        $(window).on('scroll', function() {
+            $('.fadeup').each(function() {
+                let $this = $(this);
+                let scrollTop = $(window).scrollTop();
+                let elementOffsetTop = $this.offset().top + 100;
+                let windowHeight = $(window).height();
+                if(elementOffsetTop < scrollTop + windowHeight) {
+                    $this.addClass('fadeup-on');
+                }
+            });
+        });
+    });
 });
